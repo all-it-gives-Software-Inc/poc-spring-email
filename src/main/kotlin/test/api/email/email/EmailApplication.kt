47d27efa733c3ba8bc1f93312220f05b4e2fa780
joao4018 <joao4018@gmail.com>
2022-07-26 17:55:19 -0300

    fix integration with heroku

--- src/main/kotlin/test/api/email/email/EmailApplication.kt
+++ src/main/kotlin/test/api/email/email/EmailApplication.kt
@@ -2,8 +2,10 @@ package test.api.email.email
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class EmailApplication
 
 fun main(args: Array<String>) {
