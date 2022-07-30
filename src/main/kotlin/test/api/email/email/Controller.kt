7e541ade7998a3692095c2d449909f4b50b5d1d4
joao4018 <joao4018@gmail.com>
2022-07-30 14:15:24 -0300

    enable cross origin to ms

--- src/main/kotlin/test/api/email/email/Controller.kt
+++ src/main/kotlin/test/api/email/email/Controller.kt
@@ -11,13 +11,13 @@ import test.api.email.email.services.EmailService
 /**
  * Anotação que informa ao springboot o caminho que da URL que essa classe vai responder (Ex: https://localhost:8080/api
  */
+@CrossOrigin
 @RequestMapping("/api", produces = [MediaType.APPLICATION_JSON_VALUE])
 class Controller(
         @Autowired private val emailService: EmailService,
 ) {
 
 
-    @CrossOrigin(origins = ["*"])
     @PostMapping("/email-send")
     fun sendMail(
             @RequestParam(value = "email", required = true) destinatario: String,
@@ -25,7 +25,6 @@ class Controller(
         return emailService.sendRecoveryPasswordEmail(destinatario)
     }
 
-    @CrossOrigin(origins = ["*"])
     @PostMapping("/email-validate-send")
     fun sendValidateAccountMail(
             @RequestBody(required = true) destinatario: SendEmailRequest,
